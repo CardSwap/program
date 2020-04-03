238897386a94127aa15424790834090fceb6f97b
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2020-04-03 09:01:51 -0700

    Build Data Listing

--- cardswap.py
+++ cardswap.py
@@ -1,5 +1,20 @@
 """
-CardSwap Game
-Console Program
-Made by EpicPuppy613
+CardSwap Program
+CardSwap Game: cardswap.github.io/game
+Coded by EpicPuppy613
 """
+#Imports
+import random as r
+#Data
+
+#Cards
+cards={"a":4, "two":4, "three":4, "four":4, "five":4, "six":4, "seven":4, "eight":4, "nine":4, "ten":4, "j":4, "q":4, "k":4}
+p1={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p2={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p3={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p0={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p5={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p6={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p7={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+p8={"a":0, "two":0, "three":0, "four":0, "five":0, "six":0, "seven":0, "eight":0, "nine":0, "ten":0, "j":0, "q":0, "k":0}
+#Program
